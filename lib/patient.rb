597cef0db27e2809ad332b71f551c8030d523532
Hannah Angle <hannahcangle@gmail.com>
2020-08-12 04:58:54 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,30 @@
+class Patient 
+  attr_accessor :name, :doctor, :appointment
+  
+  @@all = []
+  
+  def initialize(name) 
+    @name = name 
+    @@all << self 
+  end 
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, doctor, self)
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def appointments 
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end 
+  
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end 
+end 
+
+  
+    
+  
